4245d23e2838d052701e79fc0e4ee7cbd82b3cdf
Badziohub <blazejbadzio@gmail.com>
2023-07-18 18:03:04 +0200

    added comment

--- src/main/java/badziohub/simplecrudtemplate/firstentity/FirstEntityController.java
+++ src/main/java/badziohub/simplecrudtemplate/firstentity/FirstEntityController.java
@@ -18,6 +18,11 @@ public class FirstEntityController {
     private final FirstEntityService firstEntityService;
     private final Counter counter;
     // mogłem RestTemplate wstrzyknąć i strzelać na /counter, ale jakieś to brzydkie
+    // sortowanie odbywa się na froncie, tutaj mógłbym to zrobić dając metody w repozytorium
+    // findAllByOrderBy<nazwaPola> <asc/desc>
+    //nie wiem co miało na myśli zadanie wyszukiwanie, ale jeśli chodziło o backend to
+    // wyszukiwanie jest tak jakby po id w celu aktualizacji
+    // a jeśli na front, to byłoby to podobne do sort tylko po każdym polu tablicy items
 
     @GetMapping("")
     public List<FirstEntityResponseDto> getAll(){
