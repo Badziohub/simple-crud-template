c35c01917cf63552767f18cc650815cc3e2d3773
Badziohub <blazejbadzio@gmail.com>
2023-07-20 10:28:31 +0200

    cleanup

--- src/main/java/badziohub/simplecrudtemplate/firstentity/FirstEntityController.java
+++ src/main/java/badziohub/simplecrudtemplate/firstentity/FirstEntityController.java
@@ -1,6 +1,5 @@
 package badziohub.simplecrudtemplate.firstentity;
 
-import badziohub.simplecrudtemplate.Counter;
 import badziohub.simplecrudtemplate.firstentity.dto.FirstEntityRequestDto;
 import badziohub.simplecrudtemplate.firstentity.dto.FirstEntityResponseDto;
 import jakarta.validation.Valid;
@@ -16,35 +15,25 @@ import java.util.List;
 public class FirstEntityController {
 
     private final FirstEntityService firstEntityService;
-    private final Counter counter;
-    // mogłem RestTemplate wstrzyknąć i strzelać na /counter, ale jakieś to brzydkie
-    // sortowanie odbywa się na froncie, tutaj mógłbym to zrobić dając metody w repozytorium
-    // findAllByOrderBy<nazwaPola> <asc/desc>
-    //nie wiem co miało na myśli zadanie wyszukiwanie, ale jeśli chodziło o backend to
-    // wyszukiwanie jest tak jakby po id w celu aktualizacji
-    // a jeśli na front, to byłoby to podobne do sort tylko po każdym polu tablicy items
+
 
     @GetMapping("")
     public List<FirstEntityResponseDto> getAll(){
-        counter.updateCounter();
         return firstEntityService.getAll();
     }
 
     @PutMapping("{id}")
     public FirstEntityResponseDto update(@PathVariable Long id,@RequestBody @Valid FirstEntityRequestDto requestDto){
-        counter.updateCounter();
         return firstEntityService.update(id,requestDto);
     }
 
     @PostMapping("")
     public FirstEntityResponseDto add( @RequestBody @Valid FirstEntityRequestDto requestDto){
-        counter.updateCounter();
         return firstEntityService.add(requestDto);
     }
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable Long id){
-        counter.updateCounter();
         firstEntityService.delete(id);
     }
 
